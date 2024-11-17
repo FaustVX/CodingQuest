930217f2ac3525fc56fad0e5058a51d74ea21939
FaustVX <jodu035@gmail.com>
2024-11-17 01:15:31 +0100

    Moved `Globals` to it's own file

---
+++ CodingQuest.App/Globals.cs
@@ -0,0 +1,7 @@
+namespace CodingQuest;
+
+public static class Globals
+{
+    public static bool IsTest { get; set;}
+    public static string InputFile { get; set; } = "";
+}
--- CodingQuest.App/Program.cs
+++ CodingQuest.App/Program.cs
@@ -54,9 +54,3 @@ Console.ResetColor();
 Console.ReadLine();
 }
 #endif
-
-public static class Globals
-{
-    public static bool IsTest { get; set;}
-    public static string InputFile { get; set; } = "";
-}
