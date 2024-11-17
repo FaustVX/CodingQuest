e0fb0e9647eb27636fb536e57c5aa6bcb57d81e4
FaustVX <jodu035@gmail.com>
2024-11-17 01:08:23 +0100

    Added 2022/11/1

---
+++ CodingQuest.App/2022/11/Solution.cs
@@ -0,0 +1,33 @@
+namespace CQ_2022_11;
+
+[Day(2022, 11, id:8, "Message from home")]
+sealed partial class Solution([Field(AssignFormat = """{0}.TrimEnd()""")]string input) : ISolution
+{
+    public int RunCount => 1;
+
+    public string Run(int index)
+    {
+        var (key, range) = Globals.InputFile[^7] switch
+        {
+            '1' => ("SECRET", 26..52),
+            '2' => ("With great power comes great responsibility", ..),
+            '3' => ("With great power comes great responsibility", ..),
+            _ => ("Roads? Where We're Going, We Don't Need Roads.", ..),
+        };
+        return Run1(key, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! '()".AsSpan(range)).ToString();
+    }
+
+    public string Run1(ReadOnlySpan<char> key, ReadOnlySpan<char> charSet)
+    {
+        var output = (stackalloc char[_input.Length]);
+        var offsets = (stackalloc int[key.Length]);
+        for (int i = 0; i < key.Length; i++)
+            offsets[i] = charSet.IndexOf(key[i]);
+        for (int i = 0; i < _input.Length; i++)
+            if (!charSet.Contains(_input[i]))
+                output[i] = _input[i];
+            else
+                output[i] = charSet[(charSet.IndexOf(_input[i]) - offsets[i % offsets.Length] + charSet.Length - 1) % charSet.Length];
+        return output.ToString();
+    }
+}
